43bf289999c319046d88c388566e5d2fca9957c7
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-07-18 23:00:11 -0300

    noveno commit, debo arreglar el callback y agregar el test

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,14 @@
 class Inventory < ActiveRecord::Base
-	belongs_to :work dependent: :destroy
+	belongs_to :work, dependent: :destroy
 	validates :wheel_size, numericality: {greater_than: 0}
 	scope :custom_size, ->(n){where('wheel_size =?',n)}
 	#scope :custom_size, ->(num) { where("age > 10").mylimit(num) } 
+
+
+  	after_destroy :destroy 
+
+	def destroy
+  		r = Register.new(description: "La pieza con serial #{serial} ha sido borrada")
+  		r.save
+  	end
 end
