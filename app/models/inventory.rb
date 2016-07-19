5da9670749d362afce384c72073b20a4157c7d2e
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2016-07-18 22:41:53 -0300

    octavo commit ok

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,5 +1,5 @@
 class Inventory < ActiveRecord::Base
-	belongs_to :work
+	belongs_to :work dependent: :destroy
 	validates :wheel_size, numericality: {greater_than: 0}
 	scope :custom_size, ->(n){where('wheel_size =?',n)}
 	#scope :custom_size, ->(num) { where("age > 10").mylimit(num) } 
